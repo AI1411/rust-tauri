7ab74d794cc2f6413b2b0ca528556e07ee4d4118
AI1411 <goethe0719@gmail.com>
2022-06-18 06:59:09 +0900

    delete css

--- src/App.tsx
+++ src/App.tsx
@@ -6,42 +6,14 @@ import { invoke } from '@tauri-apps/api'
 function App() {
   const [count, setCount] = useState(0)
 
-  invoke('greet', {name: 'Akira'})
-      .then((res) => console.log(res));
+  const say_hello = (name: String) => {
+    invoke('greet', {name: name})
+        .then((res) => console.log(res));
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <button onClick={() => say_hello('test')}>test</button>
     </div>
   )
 }
